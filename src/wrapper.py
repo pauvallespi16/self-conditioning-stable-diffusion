76f159fd03e517999916b2e67d22d00c56141565
Pau Vallespi <pauvallespi16@gmail.com>
2024-05-27 12:50:52 +0200

    Refactor

---
+++ src/wrapper.py
@@ -0,0 +1,45 @@
+import os
+from typing import Dict, List
+
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+from utils import OUTPUT_IMAGES_PATH, get_module_by_name
+
+
+class ModelWrapper:
+    def __init__(
+        self,
+        model_name: str,
+        device: str,
+        save_output_images: bool = False,
+        save_activations_file: str = None,
+    ):
+        self.save_output_images = save_output_images
+        self.save_activations_file = save_activations_file
+        self.activations = []
+        if self.save_output_images:
+            os.makedirs(OUTPUT_IMAGES_PATH, exist_ok=True)
+
+    def register_hook(self, layer_name: str):
+        hook_fn = lambda module, input, output: self.activations.append(
+            output.cpu().numpy()
+        )
+        module = get_module_by_name(self.vae, layer_name)
+        module.register_forward_hook(hook_fn)
+
+    def generate_activations(self, dataloader: DataLoader) -> Dict[str, List[float]]:
+        pass
+
+    def save_reconstructed_images(
+        self,
+        decoded_images: torch.Tensor,
+        image_names: List[str],
+        output_transform: transforms.Compose,
+    ):
+        for i, image in enumerate(decoded_images):
+            output_image = output_transform(image)
+            output_image.save(
+                OUTPUT_IMAGES_PATH / f"reconstructed_{image_names[i]}.jpg"
+            )
--- src/wrapper_vae.py
+++ src/wrapper_vae.py
@@ -1,4 +1,5 @@
-import os
+from typing import Dict, List
+
 import torch
 import torchvision.transforms as transforms
 from diffusers import AutoencoderKL
@@ -6,6 +7,7 @@ from torch.utils.data import DataLoader
 
 from custom_dataset import CustomDataset
 from utils import *
+from wrapper import ModelWrapper
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL_NAME = "stabilityai/sdxl-vae"
@@ -13,7 +15,7 @@ LAYER_NAME = "decoder.mid_block.resnets.0.conv2"
 SAVE_OUTPUT_IMAGES = True
 
 
-class VariationalAutoencoderWrapper:
+class VariationalAutoencoderWrapper(ModelWrapper):
     def __init__(
         self,
         model_name: str,
@@ -21,13 +23,9 @@ class VariationalAutoencoderWrapper:
         save_output_images: bool = False,
         save_activations_file: str = None,
     ):
+        super().__init__(model_name, device, save_output_images, save_activations_file)
         self.vae = AutoencoderKL.from_pretrained(model_name).to(device)
         self.vae.eval()
-        self.save_output_images = save_output_images
-        self.save_activations_file = save_activations_file
-        self.activations = []
-        if self.save_output_images:
-            os.makedirs(OUTPUT_IMAGES_PATH, exist_ok=True)
 
     def encode_img(self, input_img: torch.Tensor) -> torch.Tensor:
         # Single image -> single latent in a batch (so size 1, 4, 64, 64)
@@ -50,26 +48,7 @@ class VariationalAutoencoderWrapper:
         image = image.detach()
         return image
 
-    def register_hook(self, layer_name: str):
-        hook_fn = lambda module, input, output: self.activations.append(
-            output.cpu().numpy()
-        )
-        module = get_module_by_name(self.vae, layer_name)
-        module.register_forward_hook(hook_fn)
-
-    def save_reconstructed_images(
-        self,
-        decoded_images: torch.Tensor,
-        image_names: list,
-        output_transform: transforms.Compose,
-    ):
-        for i, image in enumerate(decoded_images):
-            output_image = output_transform(image)
-            output_image.save(
-                OUTPUT_IMAGES_PATH / f"reconstructed_{image_names[i]}.jpg"
-            )
-
-    def generate_activations(self, dataloader: DataLoader) -> dict:
+    def generate_activations(self, dataloader: DataLoader) -> Dict[str, List[float]]:
         activations_dict = {}
         output_transform = transforms.ToPILImage()
 
@@ -97,7 +76,7 @@ class VariationalAutoencoderWrapper:
 
 def main():
     dataset = CustomDataset(
-        CAT_IMAGES_PATH, device=DEVICE, transform=get_transforms(), num_images=5
+        DOG_IMAGES_PATH, device=DEVICE, transform=get_transforms(), num_images=5
     )
     dataloader = DataLoader(dataset, batch_size=8, shuffle=False)
 
@@ -105,7 +84,7 @@ def main():
         model_name=MODEL_NAME,
         device=DEVICE,
         save_output_images=SAVE_OUTPUT_IMAGES,
-        save_activations_file=OUTPUT_PATH / "cat_activations.pkl",
+        save_activations_file=OUTPUT_PATH / "dog_activations.pkl",
     )
 
     vae.register_hook(LAYER_NAME)
